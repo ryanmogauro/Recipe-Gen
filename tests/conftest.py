485cc53aba5dcfed5823689c9b56209d10b4dc57
Ryan Mogauro <89362786+ryanmogauro@users.noreply.github.com>
2025-03-10 20:51:46 -0400

    updated reqs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 # tests/conftest.py
+import sys
+import os
 import pytest
 from unittest.mock import MagicMock
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import create_app
 
 @pytest.fixture
